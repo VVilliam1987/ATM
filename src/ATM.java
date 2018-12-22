a8cf78ecf47478224f6e930efff0455f6fc2143c
VVilliam1987 <v.moshtuk1@gmail.com>
2018-12-22 11:34:07 +0200

    Майже завершено
    Перероблено 2 завдання. Код перенесено в клас АТМ

--- src/ATM.java
+++ src/ATM.java
@@ -1,24 +1,66 @@
+import java.util.Scanner;
+
 public class ATM {
 
     int[] mValue = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+    int[] mAmount = {40, 55, 65, 80, 95, 115, 70, 45, 10};
     int mInput, mElem, mBanknote, mMoney;
+    int mUserPassword, mUserPin;
+    final double BALANCE;
+
+    public ATM(int pinCode, double balance) {
+
+        mUserPassword = pinCode;
+        BALANCE = balance;
+    }
+
+    public void start() {
+
+        Scanner id = new Scanner(System.in);
+        System.out.println("Please, enter your pin-code:");
+        mUserPin = id.nextInt();
+
+        if (mUserPin == mUserPassword) {
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Welcome!");
+            System.out.println("Please, enter the sum of money, you want to withdraw:");
+            mInput = scan.nextInt();
+            System.out.println("You want to withdraw " + mInput + " dollars.");
+
+            takeMoney();
+        } //if
+
+        else {
+            System.out.println("Incorrect login. Please, try once more");
+        } //else
+    }
 
     public void takeMoney(){
 
         mBanknote = 0;
 
-        for (int e = 0; e < mValue.length; ++e) {
+        if (mInput < BALANCE) {
 
-            mElem = mValue[e]; //номінал купюри з масиву
-            mBanknote = mInput / mElem; //кількість виданих купюр певного номіналу
-            if (mInput > 0 && mBanknote > 0) System.out.println(mElem + ": " + mBanknote);
-            mMoney += mBanknote;
-            mInput %= mElem; //залишок після видачі
+            for (int e = 0; e < mValue.length; ++e) {
 
-        } //for
+                mElem = mValue[e]; //номінал купюри з масиву
+                mBanknote = mInput / mElem; //кількість виданих купюр певного номіналу
+                if (mInput > 0 && mBanknote > 0) System.out.println(mElem + ": " + mBanknote); //if
+                mMoney += mBanknote;
+                mInput %= mElem; //залишок після видачі
 
-        System.out.println("Total amount of banknotes: " + mMoney);
+            } //for
+
+            System.out.println("Total amount of banknotes: " + mMoney);
+
+        } //if
+
+        else {
+            System.out.println("Unfortunately, you do not have this sum of money to withdraw it. Please, try once more.");
+        } //else
 
     } //takeMoney
+
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-
-    ATM atmMachine = new ATM();
-    User user = new User("1987", 19303.87);
-    Scanner id = new Scanner(System.in);
-    Scanner scan = new Scanner(System.in);
-
-        System.out.println("Please, enter your pin-code:");
-        user.mPin = id.nextLine();
-        System.out.println("Welcome!");
-        System.out.println("Please, enter the sum of money, you want to withdraw:");
-        atmMachine.mInput = scan.nextInt();
-        System.out.println("You want to withdraw " + atmMachine.mInput + " dollars.");
-
-    atmMachine.takeMoney();
+    ATM user = new ATM(1987, 19303.87);
+    user.start();
 
     }
 }
